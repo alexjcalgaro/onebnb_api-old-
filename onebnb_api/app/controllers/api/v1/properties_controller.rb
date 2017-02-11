1533b1bd9c30038a13e806275a320fab697e3358
Alex Calgaro <alexjcalgaro@gmail.com>
2017-02-10 22:23:49 -0200

    Modulo 3

--- onebnb_api/app/controllers/api/v1/properties_controller.rb
+++ onebnb_api/app/controllers/api/v1/properties_controller.rb
@@ -1,45 +1,73 @@
 class Api::V1::PropertiesController < ApplicationController
   before_action :set_api_v1_property, only: [:show, :update, :destroy, :add_to_wishlist, :remove_from_wishlist]
   before_action :authenticate_api_v1_user!, except: [:index, :show, :search]
- 
-  # GET /api/v1/properties
+
   # GET /api/v1/properties.json
   def index
     @api_v1_properties = Property.all
   end
- 
+
   # GET /api/v1/properties/1.json
   def show
   end
- 
+
   # GET /api/v1/search
   def search
     # Caso o usuário não coloque nenhuma informação pesquisamos por qualquer uma
     search_condition = params[:search] || '*'
-    
     # Caso não esteja sendo selecionado por página, pegamos a primeira
     page = params[:page] || 1
-    
     # Filtra por status, presença de wifi, máquina de lavar e etc
     # Faça você mesmo \o/
     conditions = {status: :active}
- 
+
+    if params[:wifi]
+      conditions += {wifi: params[:wifi]}
+    end
+    if params[:address_country]
+      conditions += {address_country: params[:address_country]}
+    end
+    if params[:address_city]
+      conditions += {address_city: params[:address_city]}
+    end
+    if params[:address_state]
+      conditions += {address_state: params[:address_state]}
+    end
+    if params[:washing_machine]
+      conditions += {washing_machine: params[:washing_machine]}
+    end
+    if params[:clothes_iron]
+      conditions += {clothes_iron: params[:clothes_iron]}
+    end
+    if params[:towels]
+      conditions += {towels: params[:towels]}
+    end
+    if params[:air_conditioning]
+      conditions += {air_conditioning: params[:air_conditioning]}
+    end
+    if params[:refrigerato]
+      conditions += {refrigerato: params[:refrigerato]}
+    end
+    if params[:heater]
+      conditions += {heater: params[:heater]}
+    end
+
     # Realizamos a busca do ElasticSearch
     @api_v1_properties = (Property.search search_condition, where: conditions,  page: page, per_page: 18)
     render template: '/api/v1/properties/index', status: 200
   end
- 
+
   # POST /api/v1/properties.json
   def create
     @api_v1_property = Property.new(api_v1_property_params)
- 
+
     if @api_v1_property.save
       render :show, status: :created, location: @api_v1_property
     else
       render json: @api_v1_property.errors, status: :unprocessable_entity
     end
   end
- 
+
   # PATCH/PUT /api/v1/properties/1.json
   def update
     if @api_v1_property.update(api_v1_property_params)
@@ -48,12 +76,12 @@ class Api::V1::PropertiesController < ApplicationController
       render json: @api_v1_property.errors, status: :unprocessable_entity
     end
   end
- 
+
   # DELETE /api/v1/properties/1.json
   def destroy
     @api_v1_property.destroy
   end
- 
+
   # POST /api/v1/properties/:id/wishlist.json
   def add_to_wishlist
     begin
@@ -63,7 +91,7 @@ class Api::V1::PropertiesController < ApplicationController
       render json: errors, status: :unprocessable_entity
     end
   end
- 
+
   # DELETE /api/v1/properties/:id/wishlist.json
   def remove_from_wishlist
     begin
@@ -73,13 +101,15 @@ class Api::V1::PropertiesController < ApplicationController
       render json: errors, status: :unprocessable_entity
     end
   end
- 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_api_v1_property
-      @api_v1_property = Property.find(params[:id])
+      if params[:id] != ""
+        @api_v1_property = Property.find(params[:id])
+      end
     end
- 
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def api_v1_property_params
       params.require(:api_v1_property).permit(:title, :description)
--- onebnb_api/spec/models/address_spec.rb
+++ onebnb_api/spec/models/address_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
+#   describe "POST #Address" do
+#     before do
+#         @address = create(:address)
+#     end
+
+#     context "with valid params and tokens" do
+        
+#         it "check georeference latitude in address" do
+#             post :add_to_wishlist, params: {id: @address.id}
+#             @address.reload
+#             expect(@address.latitude).not_to eql(nil)
+#         end
+
+#         it "check georeference longitude in address" do
+#             post :add_to_wishlist, params: {id: @address.id}
+#             @address.reload
+#             expect(@address.longitude).not_to eql(nil)
+#         end
+#     end
+#   end
 end
